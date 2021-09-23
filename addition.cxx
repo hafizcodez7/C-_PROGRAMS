02de65bac1d0887ceba315fd3c1a053d6d36c5c1
Hafizcode <82789669+hafizcodez7@users.noreply.github.com>
2021-09-23 07:33:10 +0530

    Add files via upload

---
+++ addition.cxx
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    
+    int a, b, c;
+    cout << "Enter a number: " << endl;
+    cin >> a;
+    cout << "Enter a nunmber: " << endl;
+    cin >> b;
+    c = (a + b);
+    cout << "Total: " << c;
+    return 0;
+}
